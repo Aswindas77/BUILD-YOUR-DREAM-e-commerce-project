a6716d05c490a89a537a8c02193cdf833dc5c54c
Aswindas77 <Aswindasachu2004@gmail.com>
2024-12-28 19:06:36 +0530

    set up view engine

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express=require('express');
 const app=express();
+const path=require('path');
 const env=require("dotenv").config();
-const db=require("./config/db")
+const db=require("./config/db");
+db();
 
-
-
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.set('views',[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')]);
+app.
 
 
  
